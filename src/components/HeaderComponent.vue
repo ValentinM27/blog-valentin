7dbbd23480f76b16171338e2dd555ab6d99fdd6e
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-08-27 19:50:08 +0200

    [setup-nav] - Init project et nav

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <img :src="mainLogo" alt="Logo principal" class="header-logo" />
+      <div>Accueil</div>
+      <div>À propos</div>
+    </div>
+    <div class="header-right">
+      <span class="header-icon-container">
+        <img :src="linkedinIcon" />
+        <img :src="instagramIcon" />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Import des ressources
+import mainLogo from 'src/assets/images/main-logo.png';
+import linkedinIcon from 'src/assets/images/linkedin-icon.png';
+import instagramIcon from 'src/assets/images/instagram-icon.png';
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <Header />
+</template>
+
+<script setup lang="ts">
+import Header from 'src/components/HeaderComponent.vue';
+
+</script>
