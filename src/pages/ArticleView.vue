803c42e6c622a60a06feb50a2053057d2ad945e4
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-08-28 15:50:49 +0200

    [fetch] - Récupération des données pour affichage article

--- src/pages/ArticleView.vue
+++ src/pages/ArticleView.vue
@@ -1,9 +1,19 @@
-<template>{{ props.idarticle }}</template>
+<template>{{ article }}</template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { ArticlesStore } from 'src/stores/articlesStore';
+import { Article } from 'src/types';
+
+// Ui const and store
+const useArticlesStore = ArticlesStore();
+const article = ref({} as Article);
 
 const props = defineProps({
   idarticle: { type: String, required: true },
+});
+
+onMounted(() => {
+  article.value = useArticlesStore.getArticlesById(parseInt(props.idarticle));
 });
 </script>
